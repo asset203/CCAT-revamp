2c2b0f87f917a545b279c01e7778b89726a14b16
Mayar <mayar.ezzeldin@asset.com.eg>
2024-03-03 18:15:05 +0200

    rm

--- Gateway issue/No changes/gateway-service/src/main/java/com/asset/ccat/gateway/models/requests/admin/dynamic_page/GetAllDynamicPagesRequest.java
+++
@@ -1,7 +0,0 @@
-package com.asset.ccat.gateway.models.requests.admin.dynamic_page;
-
-import com.asset.ccat.gateway.models.requests.BaseRequest;
-
-public class GetAllDynamicPagesRequest extends BaseRequest{
-
-}
--- Gateway issue/No changes/gateway-service/src/test/java/com/asset/ccat/gateway/MainApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.asset.ccat.gateway;
-class MainApplicationTests {
-
-    //@Test
-    void contextLoads() {
-    }
-
-}
