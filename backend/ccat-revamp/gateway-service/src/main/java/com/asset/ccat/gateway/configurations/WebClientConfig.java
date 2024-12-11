11e6f97451bb833f56c2280ec1d3c37800144798
Mayar <mayar.ezzeldin@asset.com.eg>
2024-12-11 13:15:05 +0200

    webclient buffer size 10MB

--- backend/ccat-revamp/gateway-service/src/main/java/com/asset/ccat/gateway/configurations/WebClientConfig.java
+++ backend/ccat-revamp/gateway-service/src/main/java/com/asset/ccat/gateway/configurations/WebClientConfig.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 
@@ -43,8 +44,12 @@ public class WebClientConfig {
                         .addHandlerLast(new ReadTimeoutHandler(properties.getReadTimeout(), TimeUnit.MILLISECONDS))
                         .addHandlerLast(new WriteTimeoutHandler(properties.getWriteTimeout(), TimeUnit.MILLISECONDS))
                 );
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024)) // 10 MB
+                .build();
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .exchangeStrategies(exchangeStrategies)
                 .build();
     }
 }
