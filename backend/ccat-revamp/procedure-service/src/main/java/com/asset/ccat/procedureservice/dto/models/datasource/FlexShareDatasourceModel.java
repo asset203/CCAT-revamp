58b2619b58393c11a910893e770a4b232f1b9a79
Mayar <mayar.ezzeldin@asset.com.eg>
2024-12-23 16:56:16 +0200

    rm

--- backend/ccat-revamp/procedure-service/src/main/java/com/asset/ccat/procedureservice/dto/models/datasource/FlexShareDatasourceModel.java
+++
@@ -1,9 +0,0 @@
-package com.asset.ccat.procedureservice.dto.models.datasource;
-
-public class FlexShareDatasourceModel extends BasicDataSourceModel{
-    public FlexShareDatasourceModel() {
-    }
-    public FlexShareDatasourceModel(String URL, String username, String password, String schema) {
-        super(URL, username, password, schema);
-    }
-}
